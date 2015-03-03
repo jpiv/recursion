d9ef6e259b23e0777403b65662a4aca477531f02
John Pizzo <john@JOHN-DESKTOP.pizzoware.com>
2015-03-03 00:27:55 -0500

    Implemented stringifyJSON

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -5,4 +5,41 @@
 
 var stringifyJSON = function(obj) {
   // your code goes here
+  var stringifyed = "";
+
+  if(Array.isArray(obj))
+  {
+    stringifyed += '[';
+    stringifyed += obj[0] ? stringifyJSON(obj[0]) : '';
+    for(var i = 1; i < obj.length; i++)
+    {
+      stringifyed += ',' + stringifyJSON(obj[i]);
+    }
+    stringifyed += ']';
+  }
+  else if(typeof obj === 'object' && obj !== null)
+  {
+    var firstTime = true;
+    stringifyed += '{';
+	  for(var key in obj)
+	  {
+      if (!(obj[key] instanceof Function) && obj[key] !== undefined)
+      {
+        if (firstTime)
+        {
+  	  	  stringifyed += '"' + key + '"' + ':' + stringifyJSON(obj[key]);
+          firstTime = false;
+        }
+        else
+          stringifyed += ',"' + key + '"' + ':' + stringifyJSON(obj[key]);
+      }
+	  }
+    stringifyed += '}';
+  }
+  else if(typeof obj === "string")
+    stringifyed += '"' + obj + '"';
+  else
+    stringifyed += obj;
+
+  return stringifyed;
 };
